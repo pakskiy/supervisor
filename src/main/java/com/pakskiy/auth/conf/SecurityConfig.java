6ee065890e29350320b11e0a858a5d51f696e1a6
Oleg <oleg.pak@paynet.uz>
2024-06-13 09:42:39 +0500

    Refactoring

--- src/main/java/com/pakskiy/auth/conf/SecurityConfig.java
+++ src/main/java/com/pakskiy/auth/conf/SecurityConfig.java
@@ -1,80 +1,30 @@
 package com.pakskiy.auth.conf;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import java.net.URI;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
-@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler) throws Exception {
-        return http.authorizeHttpRequests(authorise ->
-                        authorise
-                                .requestMatchers("/static/**", "/templates/**")
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated())
-                .csrf(AbstractHttpConfigurer::disable)
-                .exceptionHandling(authorise -> authorise.accessDeniedPage("/access-denied"))
-                .oauth2Login(withDefaults())
-                .logout(logout ->
-                        logout.logoutSuccessHandler(oidcLogoutSuccessHandler)).build();
-    }
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/public").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 
-    @Bean
-    @SuppressWarnings("unchecked")
-    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
-        return (authorities) -> {
-            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-
-            authorities.forEach(authority -> {
-                if (authority instanceof OidcUserAuthority oidcUserAuthority) {
-                    OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
-                    Map<String, Object> realmAccess = userInfo.getClaim("realm_access");
-                    Collection<String> realmRoles;
-                    if (realmAccess != null
-                            && (realmRoles = (Collection<String>) realmAccess.get("roles")) != null) {
-                        realmRoles
-                                .forEach(role -> mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
-                    }
-                }
-            });
-            return mappedAuthorities;
-        };
-    }
-
-    @Bean
-    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler(ClientRegistrationRepository clientRegistrationRepository) {
-        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri(URI.create("http://localhost:8080").toString());
-        return successHandler;
-    }
-
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+        return http.build();
     }
 }
