58e82fe51a6de1ca68883feaafe9a03866c136bf
oleg.pak <pakskiy@gmail.com>
2024-07-01 19:03:33 +0500

    First init

--- src/main/java/com/pakskiy/auth/conf/KeycloakConfig.java
+++ src/main/java/com/pakskiy/auth/conf/KeycloakConfig.java
@@ -1,4 +1,34 @@
 package com.pakskiy.auth.conf;
 
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class KeycloakConfig {
+    @Value("${keycloak.clientId}")
+    private String clientId;
+
+    @Value("${keycloak.clientSecret}")
+    private String adminClientSecret;
+
+    @Value("${keycloak.urls.auth}")
+    private String authServerUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Bean
+    public Keycloak keycloak(){
+        return KeycloakBuilder.builder()
+                .serverUrl(authServerUrl)
+                .realm(realm)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .clientId(clientId)
+                .clientSecret(adminClientSecret)
+                .build();
+    }
 }
--- src/main/java/com/pakskiy/auth/conf/SecurityConfig.java
+++ src/main/java/com/pakskiy/auth/conf/SecurityConfig.java
@@ -1,80 +1,122 @@
 package com.pakskiy.auth.conf;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import java.net.URI;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcReactiveOAuth2UserService;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
 @Configuration
 @EnableWebFluxSecurity
-@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
+    private final String[] publicRoutes = {"/api/v1/auth/register/**", "api/v1/auth/login"};
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler) throws Exception {
-        return http.authorizeHttpRequests(authorise ->
-                        authorise
-                                .requestMatchers("/static/**", "/templates/**")
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated())
-                .csrf(AbstractHttpConfigurer::disable)
-                .exceptionHandling(authorise -> authorise.accessDeniedPage("/access-denied"))
-                .oauth2Login(withDefaults())
-                .logout(logout ->
-                        logout.logoutSuccessHandler(oidcLogoutSuccessHandler)).build();
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(configurer -> configurer
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .pathMatchers(publicRoutes).permitAll()
+                        .pathMatchers("/api/v1/auth/info/**").authenticated()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(customizer -> customizer.jwt(jwt -> {
+                    ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+                    jwtAuthenticationConverter.setPrincipalClaimName("preferred_username");
+                    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+                    // Custom converter for the nested roles
+                    Converter<Jwt, Collection<GrantedAuthority>> customConverter = jwt2 -> {
+                        Map<String, Object> realmAccess = (Map<String, Object>) jwt2.getClaims().get("realm_access");
+                        if (realmAccess == null || realmAccess.isEmpty()) {
+                            return new ArrayList<>();
+                        }
+                        Collection<String> roles = (Collection<String>) realmAccess.get("roles");
+                        return roles.stream()
+                                .map(roleName -> "ROLE_" + roleName) // Prefixing role name with "ROLE_"
+                                .map(SimpleGrantedAuthority::new)
+                                .collect(Collectors.toList());
+                    };
+
+                    // Combine the default and custom converters
+                    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                            new ReactiveJwtGrantedAuthoritiesConverterAdapter(jwt3 -> {
+                                Stream<GrantedAuthority> defaultAuthorities = jwtGrantedAuthoritiesConverter.convert(jwt3).stream();
+                                Stream<GrantedAuthority> customAuthorities = Objects.requireNonNull(customConverter.convert(jwt3)).stream();
+                                return Stream.concat(defaultAuthorities, customAuthorities).collect(Collectors.toList());
+                            })
+                    );
+
+                    // Set the custom JWT authentication converter
+                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter);
+
+
+                }))
+
+                .exceptionHandling(exceptionHandlingConfigurer -> exceptionHandlingConfigurer
+                        .authenticationEntryPoint((swe, e) -> {
+                            log.error("IN securityWebFilterChain - unauthorized error: {}", e.getMessage());
+                            return Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED));
+                        })
+                        .accessDeniedHandler((swe, e) -> {
+                            log.error("IN securityWebFilterChain - access denied: {}", e.getMessage());
+
+                            return Mono.fromRunnable(() -> swe.getResponse().setStatusCode(HttpStatus.FORBIDDEN));
+                        }))
+                .build();
+
+
+//                .authorizeHttpRequests(authorise ->
+//                        authorise
+//                                .requestMatchers("/static/**", "/templates/**")
+//                                .permitAll()
+//                                .anyRequest()
+//                                .authenticated())
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .exceptionHandling(authorise -> authorise.accessDeniedPage("/access-denied"))
+//                .oauth2Login(withDefaults())
+//                .logout(logout ->
+//                        logout.logoutSuccessHandler(oidcLogoutSuccessHandler)).build();
     }
 
     @Bean
-    @SuppressWarnings("unchecked")
-    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
-        return (authorities) -> {
-            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-
-            authorities.forEach(authority -> {
-                if (authority instanceof OidcUserAuthority oidcUserAuthority) {
-                    OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
-                    Map<String, Object> realmAccess = userInfo.getClaim("realm_access");
-                    Collection<String> realmRoles;
-                    if (realmAccess != null
-                            && (realmRoles = (Collection<String>) realmAccess.get("roles")) != null) {
-                        realmRoles
-                                .forEach(role -> mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
-                    }
-                }
-            });
-            return mappedAuthorities;
-        };
-    }
-
-    @Bean
-    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler(ClientRegistrationRepository clientRegistrationRepository) {
-        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri(URI.create("http://localhost:8080").toString());
-        return successHandler;
-    }
-
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    public ReactiveOAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        OidcReactiveOAuth2UserService oidcUserService = new OidcReactiveOAuth2UserService();
+        return userRequest -> oidcUserService.loadUser(userRequest)
+                .map(oidcUser -> {
+                    List<GrantedAuthority> grantedAuthorities = Stream.concat(oidcUser.getAuthorities().stream(),
+                                    oidcUser.getClaimAsStringList("roles").stream()
+                                            .filter(authority -> authority.startsWith("ROLE_"))
+                                            .map(SimpleGrantedAuthority::new))
+                            .toList();
+                    return new DefaultOidcUser(grantedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo(),
+                            "preferred_username");
+                });
     }
 }
